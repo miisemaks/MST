4edca35b1209b2621333ed0e54601e9ea61d05d5
miisemaks <miise95mmaks@mail.ru>
2026-02-05 14:07:26 +0900

    eslint setup fsd

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
