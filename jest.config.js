12c4bddb8891916ba6fd9f4e6d86f58305f8bfaf
miisemaks <miise95mmaks@mail.ru>
2026-02-05 14:03:01 +0900

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|@react-navigation/native|@react-navigation/native-stack|@react-navigation/bottom-tabs)/)'
+  ],
+  setupFiles: [
+    './jest.setup.js'
+  ],
 };
